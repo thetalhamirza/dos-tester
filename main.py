c620ab96dcda72acf13864bdd9b88da739d6dba6
thetalhamirza <talhaavgstudent@gmail.com>
2025-01-31 20:44:40 +0500

    Add main file

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+import time
+from scapy.all import Ether, IP, TCP, sendp
+
+TARGET_IP = "192.168.x.x"       # Enter target IP here
+INTERFACE = "eth0"              # Enter your interface to use
+NUM_PACKETS = 100
+DURATION = 5
+
+def send_packets(target_ip, interface, num_packets, duration):
+    packet = Ether() / IP(dst=target_ip) / TCP()
+    end_time = time.time() + duration
+    packet_count = 0
+
+    while time.time() < end_time and packet_count < num_packets:
+        sendp(packet, iface=interface)
+        packet_count += 1
+
+
+def main():
+    if sys.version_info[0] < 3:
+        print("This script requires Python 3.")
+        sys.exit(1)
+
+    
+
+    send_packets(TARGET_IP, INTERFACE, NUM_PACKETS, DURATION)
+
+
+if __name__ == "__main__":
+    main()
